a7cbfd1e8b54975955044c6e14b0473be598e875
aznaqCre18 <azizaqibs@gmail.com>
2020-05-30 00:28:10 +0700

    build

---
+++ docs/precache-manifest.8b251a0939c4dbdd2aaaf9c690ec8cc6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc3d7ef92d3b58015d8fbdd15ec4d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b682afaa9935589b34bc",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "98cd0eb6dc7487979d84",
+    "url": "/static/css/main.3395ed17.chunk.css"
+  },
+  {
+    "revision": "b682afaa9935589b34bc",
+    "url": "/static/js/2.f00de279.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/static/js/2.f00de279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cd0eb6dc7487979d84",
+    "url": "/static/js/main.af594c7c.chunk.js"
+  },
+  {
+    "revision": "c19db5344aaebcc102c8",
+    "url": "/static/js/runtime-main.ab87ae70.js"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "927b0f6c48aed2e4846fb83c0b3d389e",
+    "url": "/static/media/banner-bg.927b0f6c.svg"
+  },
+  {
+    "revision": "b6697f1d62d765a0258ffe768387cd1e",
+    "url": "/static/media/bg-logo.b6697f1d.svg"
+  },
+  {
+    "revision": "f382d3f26e93ea98ffe1e00bb6a4fdc8",
+    "url": "/static/media/bg-texture.f382d3f2.svg"
+  },
+  {
+    "revision": "dfab2bf9070e0eddfd75a0900a7e2d95",
+    "url": "/static/media/check-mobile.dfab2bf9.svg"
+  },
+  {
+    "revision": "496edf501616b3f4a56127824513b5d5",
+    "url": "/static/media/check.496edf50.svg"
+  },
+  {
+    "revision": "4cc1d8b51cbd3e7855beca272ac7a28f",
+    "url": "/static/media/cross.4cc1d8b5.svg"
+  },
+  {
+    "revision": "8a5df041689e174eb6d42f4b624b3cd8",
+    "url": "/static/media/facebook.8a5df041.svg"
+  },
+  {
+    "revision": "bf01ec17698f49989b6adf925f7b968c",
+    "url": "/static/media/hero-landing.bf01ec17.svg"
+  },
+  {
+    "revision": "560923935e2b8d389101a2985509c7fc",
+    "url": "/static/media/instagram.56092393.svg"
+  },
+  {
+    "revision": "3bccb3fc91901548037a5de61c173b87",
+    "url": "/static/media/login-hero.3bccb3fc.svg"
+  },
+  {
+    "revision": "9ca775490140da244c23e4250ecd10f8",
+    "url": "/static/media/logo-name.9ca77549.svg"
+  },
+  {
+    "revision": "88f1ae0ca59b971d0ce6daab1f305304",
+    "url": "/static/media/twitter.88f1ae0c.svg"
+  }
+]);
